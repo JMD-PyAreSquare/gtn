5937023b50ec8149c06a340a9ddeae562481d63c
JMD-PyAreSquare <109446599+JMD-PyAreSquare@users.noreply.github.com>
2022-08-16 10:14:56 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -12,23 +12,61 @@ root.config(bg='lightblue')
 root.geometry('250x110')
 root.minsize(250, 110)
 root.maxsize(250, 110)
+guesses = []
+
+def sizereset():
+    global popupwindow
+    global sizebox
+    try:
+        maxfile = open('assets/max.txt', 'w')
+        maxfile.write(sizebox.get())
+        maxfile.close()
+    except FileNotFoundError:
+        maxfile = open('assets/maxfile.txt', "x")
+        maxfile.write(sizebox.get())
+        maxfile.close()
+    popupwindow.destroy()
+    reset()
+
+def popup():
+    global popupwindow
+    global sizebox
+    popupwindow = Toplevel(root)
+    popupwindow.title("Settings")
+    popupwindow.geometry("200x75")
+    popupwindow.maxsize(200,75)
+    popupwindow.minsize(200,75)
+    popupwindow.config(bg='lightblue')
+    sizebox = Entry(popupwindow, width=30, bd=3,bg='skyblue')
+    button1 = Button(popupwindow, text = "Ok",bg='skyblue', command = sizereset)
+    sizebox.pack(padx=5,pady=5)
+    button1.pack(padx=5,pady=5)
+    popupwindow.mainloop()
 
 def check():
     global tries
+    global guesses
+    global lower
+    global higher
+
     guess = guessbox.get()
 
     try:
         guess = int(guess)
         if guess == number:
-            tries += 1
             lb1.config(text='You guessed correctly!')
             lb2.config(text=f'You took {tries} tries.')
         elif guess < number:
-            tries += 1
+            lower = guess
+            lb1.config(text=f'Guess a number between {lower} and {higher},')
             lb2.config(text=f'pick a number that is greater than {guess}')
         elif guess > number:
-            tries += 1
+            higher = guess
+            lb1.config(text=f'Guess a number between {lower} and {higher},')
             lb2.config(text=f'pick a number that is less than {guess}')
+        if guess not in guesses:
+            guesses.append(guess)
+            tries += 1
     except:
         lb2.config(text='choose an INTEGER')
     guessbox.delete(0, END)
@@ -37,10 +75,13 @@ def reset():
     global maximum
     global number
     global tries
+    global guesses
+    global lower
+    global higher
 
-    
+    guesses.clear()
     errorcheck = 0
-
+    
     try:
         maxfile = open('assets/max.txt')
         maximum = int(maxfile.read())
@@ -52,9 +93,11 @@ def reset():
         maxfile.write('100')
         maxfile.close()
         errorcheck = 1
-        
+
+    lower = 0   
     tries = 0
     number = random.randint(0, maximum)
+    higher = maximum
 
     lb1.config(text=f'Guess a number between 0 and {maximum},')
     lb2.config(text='use the texbox and button to guess.')
@@ -64,7 +107,14 @@ def reset():
 maximum = 0
 number = 0
 tries = 0
+lower = 0
+higher = maximum
 
+menubar = Menu(root)
+root.config(menu=menubar)
+options = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="options", menu=options)
+options.add_command(label='change limit', command = popup)
 
 lb1 = Label(root, text=f'Guess a number between 0 and {maximum},',bg='lightblue')
 lb1.pack()
@@ -78,5 +128,4 @@ gamereset = Button(root,bd=3, text='Reset the game.',bg='skyblue', command=reset
 gamereset.pack(side='right', expand=True)
 
 reset()
-
 root.mainloop()
